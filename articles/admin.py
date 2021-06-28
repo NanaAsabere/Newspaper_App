fbfa5966b261fe75f6aa3bed4cbc7215e2ac0ad0
Nana Asabere <asabere.ernest@gmail.com>
2021-06-28 20:20:49 +0000

    added newspaper app here

---
+++ articles/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from .models import Article, Comment
+
+
+class CommentInline(admin.StackedInline):
+	model = Comment
+
+class ArticleAdmin(admin.ModelAdmin):
+	inlines = [
+		CommentInline,
+	]
+
+admin.site.register(Article, ArticleAdmin)
+
+admin.site.register(Comment)
+
+# Register your models here.
